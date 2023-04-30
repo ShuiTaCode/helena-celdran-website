421731a0e4d1d4c1bb6f4e12a96a43798ea960ac
Mario <mario.quitt@gmail.com>
2023-04-30 19:33:26 +0200

    Initial commit

---
+++ src/Topbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex-row">
+    <div class="top-bar-item">WORK</div>
+    <div class="top-bar-item">ABOUT</div>
+    <div class="top-bar-item">CONTACT</div>
+    <div class="top-bar-item" style="margin-right: 2rem">
+      <i class="pi pi-search"></i>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.flex-row {
+  width: 100vw;
+  height: 10vh;
+  background-color: transparent;
+  display: flex;
+  justify-content: flex-end;
+}
+.top-bar-item {
+  margin: 1rem;
+}
+</style>
